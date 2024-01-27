8023ceb499e1224a658de56fad24169d73e6ea4b
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-01-27 13:31:26 -0500

    short circuiting examples added

--- script.js
+++ script.js
@@ -199,3 +199,27 @@ pagesRange;
 console.log(`The book has ${pagesRange} pages`);
 
 console.log(getYear(publicationDate));
+
+console.log(true && "Some string");
+console.log(false && "Some string");
+console.log(hasMovieAdaptation && "This book has a movie");
+
+// falsy values: 0, '', null, undefined
+
+console.log("jonas" && "Some string");
+console.log(0 && "Some string");
+
+console.log(true || "Some string");
+console.log(false || "Some string");
+
+console.log(book.translations.spanish);
+
+const spanishTranslation = book.translations.spanish || "NOT TRANSLATED";
+spanishTranslation;
+
+console.log(book.reviews.librarything.reviewsCount);
+const countWrong = book.reviews.librarything.reviewsCount || "no data";
+countWrong;
+
+const count = book.reviews.librarything.reviewsCount ?? "no data";
+count;
