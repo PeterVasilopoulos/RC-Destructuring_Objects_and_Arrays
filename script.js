714b540b4435bd25e55a12f790d3abb6dfec21a7
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-01-29 12:15:26 -0500

    added examples of how to work with immutable arrays

--- script.js
+++ script.js
@@ -293,3 +293,24 @@ arr
 
 const sortedByPages = books.slice().sort((a,b) => a.pages - b.pages)
 sortedByPages
+
+
+
+// Working With Immutable Arrays
+
+// 1. add book object to array
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J.K. Rowling"
+}
+const booksAfterAdd = [...books, newBook]
+booksAfterAdd
+
+// 2. delete book object from array
+const booksAfterDelete = booksAfterAdd.filter(book => book.id !== 3)
+booksAfterDelete
+
+// 3. update book object in array
+const booksAfterUpdate = booksAfterDelete.map(book => book.id === 1 ? {...book, pages: 1210} : book)
+booksAfterUpdate
