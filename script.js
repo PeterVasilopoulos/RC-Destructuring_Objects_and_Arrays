ec5af6726ebb03f086a2c87f5e1df05de7ff613a
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2024-01-29 12:28:55 -0500

    asynchronous javascript async/await example added

--- script.js
+++ script.js
@@ -324,8 +324,21 @@ booksAfterUpdate
 
 
 // Asynchronous JavaScript: Promises
-fetch("https://jsonplaceholder.typicode.com/todos")
-  .then(res => res.json())
-  .then(data => console.log(data))
 
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then(res => res.json())
+//   .then(data => console.log(data))
+
+// console.log("jonas")
+
+
+// Asynchronous JavaScript: Async/Await
+
+async function getTodos() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/todos")
+  const data = await res.json()
+  console.log(data)
+}
+
+getTodos()
 console.log("jonas")
